248bea7fa1e5bfffa3f1e7cc943cee9f0721318f
Remy Skelton <remy.skelton@stu.ocsb.ca>
2025-03-16 23:37:20 +0000

    Done linting

---
+++ package-info.java
@@ -0,0 +1,17 @@
+/**
+ * This is the package-info.java file for the FileIO package.
+ * 
+* The FileIO program will read from the input.txt file with integers,
+* decimal and non numerics on each line.
+* It will then try to convert each of the different data types
+* from a string to an integer.
+* If it is successful, it will add the integer to the sum and
+* display the sum of the integers on the line.
+* If it is not successful, it will display an error message.
+* If there are no integers on the line, it will display an error message.
+* It will then write the output to the output.txt file.
+*
+* @author Remy Skelton
+* @version 1.0
+* @since 2025-03-07
+*/
